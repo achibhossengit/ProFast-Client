88d9b70baee1294d2a71f3fe3d697152dd8d6cea
achibhossengit <mail.achibhossen@gmail.com>
2025-09-22 15:15:59 +0600

    add hover design for services and process section

--- src/pages/Home/Process/ProcessCard.jsx
+++ src/pages/Home/Process/ProcessCard.jsx
@@ -1,7 +1,7 @@
 const ProcessCard = ({ step }) => {
     const {icon: Icon, title, description} = step
   return (
-    <div className="card bg-base-100 shadow-md rounded-xl p-6 flex flex-row items-start gap-4">
+    <div className="card bg-base-100 hover:bg-primary transition duration-300 shadow-md rounded-xl p-6 flex flex-row items-start gap-4">
       <div>
         <Icon className="text-3xl "></Icon>
         <h3 className="text-lg font-semibold text-secondary">{title}</h3>
--- src/pages/Home/Service/ServiceCard.jsx
+++ src/pages/Home/Service/ServiceCard.jsx
@@ -1,9 +1,11 @@
 const ServiceCard = ({ service }) => {
   const { icon: Icon, title, description } = service;
   return (
-    <div className="card bg-base-100 shadow-lg rounded-2xl ">
+    <div className="card bg-base-100 shadow-lg rounded-2xl hover:bg-primary transition duration-300">
       <div className="card-body items-center text-center">
-        <Icon className="text-4xl text-primary"></Icon>
+        <div className="p-5 bg-base-100 rounded-full">
+          <Icon className="text-4xl text-primary"></Icon>
+        </div>
         <h3 className="text-xl font-semibold mt-4">{title}</h3>
         <p className="mt-2 text-sm leading-relaxed">{description}</p>
       </div>
